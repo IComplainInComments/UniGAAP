c8efdfbb7e3e3662c4a168bce43b61d445744f3c
IComplainInComments <IComplainInComments@users.noreply.github.com>
2023-05-13 04:34:59 -0400

    checkin.

--- UniGAAP/blastbox/FileHandler.java
+++ UniGAAP/blastbox/FileHandler.java
@@ -5,61 +5,155 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
 
+/**
+ * The FileHandler class is responsible for taking a text file and inputting the data
+ * to be used inside this program.
+ * 
+ * @author IComplainInComments
+ * @version 1.0
+ */
 public class FileHandler {
     private File target;
     private BufferedReader buff;
+    private ByteBuffer raw, normalized;
     private String charSet;
-    private CompletableFuture<List<String>> readText;
-    private CompletableFuture<List<String>> convertText;
+    private CompletableFuture<Void> readText;
+    private CompletableFuture<Void> convertText;
+    /**
+     * Constructor for the class.
+     * 
+     * Checks for the file's existence.
+     * @param fileName String
+     */
     public FileHandler(String fileName){
         this.target = new File(fileName);
         if(this.target.exists()){
             readFile();
         } else {
-            System.err.println("Error: File does not exist!");
+            System.err.println("ERROR: File does not exist, or is not at this location");
             System.exit(0);
         }
     }
+    /**
+     * readFile() method takes the file location, and opens a file reader.
+     * The method also sets the variable charSet to the targeted files encoding
+     * and creates a BufferedReader to process the file.
+     */
     private void readFile(){
         try {
         FileReader reader = new FileReader(this.target);
             this.charSet = reader.getEncoding();
             this.buff = new BufferedReader(reader, (int)this.target.length());
+            this.raw = ByteBuffer.allocate((int)target.length());
+            processFile();
         } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
             e.printStackTrace();
             System.exit(0);
         }
-        this.readText = CompletableFuture.supplyAsync(() -> {
-            List<String> temp = buff.lines().parallel().collect(Collectors.toList());
-            return temp;
+
+    }
+    /**
+     * processFile() Method uses CompleteableFutures to process the file.
+     * Data is stored in Char/Byte Buffers.
+     * @see java.util.concurrent.CompletableFuture
+     * @see java.nio.ByteBuffer
+     * @see java.nio.CharBuffer
+     */
+    private void processFile(){
+        
+        //This first future reads the texts and places it into a CharBuffer called raw.
+        this.readText = CompletableFuture.runAsync(() -> new Runnable() {
+            @Override
+            public void run(){
+                try {
+                    buff.read(raw.asCharBuffer());
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
+                    e.printStackTrace();
+                }
+            }
         });
+        //Check if the file needs normalized.
         if(this.charSet == StandardCharsets.UTF_16.name()){
-            this.convertText = this.readText.thenApplyAsync(func ->  temp = func.parallelStream().forEach(null));
+            this.normalized = ByteBuffer.allocate((int)target.length());
+            //This second future converts the data from its current encoding into UTF-16 and is stored in the normalized buffer.
+            this.convertText = CompletableFuture.runAsync(() -> new Runnable() {
+                @Override
+                public void run(){
+                    Charset charset = Charset.forName("UTF-16");
+                    CharsetEncoder encoder = charset.newEncoder();
+                    try {
+                        normalized = encoder.encode(raw.asCharBuffer());
+                    } catch (CharacterCodingException e) {
+                        System.err.println(e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
+            });
 
         }
     }
+    /**
+     * Gives Characterset.
+     * @return String
+     */
     public String getCharset(){
         return this.charSet;
     }
-    public Long getFileLength(){
+    /**
+     * Gives the file length.
+     * @return long
+     * @see java.io.File
+     */
+    public long getFileLength(){
         return this.target.length();
     }
-    public boolean buffReady() throws IOException{
-        return this.buff.ready();
-    }
-    public boolean fileReady(){
+    /**
+     * Checks if the file is now in a buffer.
+     * @return boolean
+     */
+    public boolean rawTextReady(){
         return this.readText.isDone();
     }
-    public void closeBuff() throws IOException{
-        this.buff.close();
+    /**
+     * Checks if the text normalization is complete.
+     * @return boolean
+     */
+    public boolean normalizedTextReady(){
+        return this.convertText.isDone();
     }
-    public List<String> getRawText() throws InterruptedException, ExecutionException{
-        return this.readText.get();
+    /**
+     * Empties the buffer.
+     */
+    public void clearRawBuffer(){
+        this.raw.clear();
+    }
+    /**
+     * Empties the buffer.
+     */
+    public void clearNormalizedBuffer(){
+        this.normalized.clear();
+    }
+    /**
+     * Gives the buffer with the raw text.
+     * @return CharBuffer
+     */
+    public ByteBuffer getRawText(){
+        return this.raw;
+    }
+    /**
+     * Gives the buffer with the converted text.
+     * @return CharBuffer
+     */
+    public ByteBuffer getNormalizedText(){
+        return this.normalized;
     }
 }
--- UniGAAP/database/Database.java
+++ UniGAAP/database/Database.java
@@ -1,10 +1,19 @@
 package UniGAAP.database;
 
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.OutputStreamWriter;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 public class Database{
     File database;
     DatabaseMetaData meta;
@@ -12,24 +21,87 @@ public class Database{
     
     public Database(String fileName){
         this.database = new File(fileName);
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:"+fileName)) {
+        try (this.conn = DriverManager.getConnection("jdbc:sqlite:"+fileName)) {
             if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("The driver name is " + meta.getDriverName());
-                System.out.println("A new database has been created.");
+                this.meta = conn.getMetaData();
+                System.out.println("INFO::The driver name is " + meta.getDriverName()+".");
+                System.out.println("INFO::A new database has been created.");
+                String table = "CREATE TABLE IF NOT EXISTS literature (\n"
+                + "	id INTEGER PRIMARY KEY,\n"
+                + "	name TEXT NOT NULL,\n"
+                + " date INTEGER NOT NULL,\n"
+                + " raw BLOB NOT NULL,\n"
+                + " normalized BLOB,\n"
+                + "	aux1 BLOB,\n"
+                + " aux2 BLOB\n"
+                + ");";
+                createTable(table);
+                System.out.println("INFO::Made standard table in new database.");
             }
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
-    public void createTable(){
+    public void createTable(String query){
+        try (Connection conn = DriverManager.getConnection(database.getAbsolutePath());
+                Statement stmt = conn.createStatement()) {
+            stmt.execute(query);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
-    public void deleteTable(){
+    public void deleteTable(String query){
+        try (Connection conn = DriverManager.getConnection(database.getAbsolutePath());
+                Statement stmt = conn.createStatement()) {
+            stmt.execute(query);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void readData(String query){
+        try (Connection conn = DriverManager.getConnection(database.getAbsolutePath());
+                Statement stmt = conn.createStatement()) {
+            stmt.execute(query);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void writeData(String table, String name, int dateTime, Charset charSet, ByteBuffer data1, ByteBuffer data2){
+        String sql = "INSERT INTO ";
+        if(!table.isBlank() || !table.isEmpty()){
+            sql += table;
+        } else {
+            return;
+        }
+        if(!name.isBlank() || !name.isEmpty()){
+            sql += name;
+        } else {
+            return;
+        }
+        if(dateTime > 0){
+            sql += dateTime;
+        }
+        if(data1 != null && data2 != null){
+
+        } else if(data1 != null && data2 == null){
+
+        } else if(data1 == null && data2 != null){
+
+        }else{
+
+        }
+
 
     }
-    public void query(){
-
+    public void customQuery(String query){
+        try (Connection conn = DriverManager.getConnection(database.getAbsolutePath());
+                Statement stmt = conn.createStatement()) {
+            stmt.execute(query);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
+    
 }
